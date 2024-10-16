0cb70ee013a5cd5c19ca9f6ba11ef7870ec12c5c
Dumitru Vivat <kaibaman94@gmail.com>
2024-10-16 17:48:59 +0300

    first commit

---
+++ src/main/java/com/example/copyoftwitter/CopyOfTwitterApplication.java
@@ -0,0 +1,13 @@
+package com.example.copyoftwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopyOfTwitterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CopyOfTwitterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/copyoftwitter/CopyOfTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.copyoftwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopyOfTwitterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
