fc32f2e87bfebb0188dccd47c711d17f7f65220b
Dumitru Vivat <kaibaman94@gmail.com>
2024-10-17 16:51:21 +0300

    add freemarker

--- src/main/java/com/example/copyoftwitter/controller/MainController.java
+++ src/main/java/com/example/copyoftwitter/controller/MainController.java
@@ -6,9 +6,11 @@ import com.example.copyoftwitter.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
@@ -19,15 +21,24 @@ public class MainController {
     private MessageRepository messageRepository;
 
     @GetMapping("/")
-    public String greeting(Map<String, Object> model) {
+    public String greeting(Model model) {
         return "greeting";
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
+    public String main(
+            @RequestParam(required = false, defaultValue = "") String filter,
+            Model model) {
         Iterable<Message> messages = messageRepository.findAll();
 
-        model.put("messages",messages);
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+
+        model.addAttribute("messages",messages);
+        model.addAttribute("filter",filter);
         return "main";
     }
 
@@ -36,28 +47,13 @@ public class MainController {
             @AuthenticationPrincipal User user,
             @RequestParam String text,
             @RequestParam String tag,
-            Map<String, Object> model) {
+            Model model) {
 
         var message = new Message(text, tag, user);
         messageRepository.save(message);
 
         Iterable<Message> messages = messageRepository.findAll();
-        model.put("messages",messages);
-
-        return "main";
-    }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter,
-                      Map<String, Object> model) {
-        Iterable<Message> messages;
-
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepository.findByTag(filter);
-        } else {
-            messages = messageRepository.findAll();
-        }
-        model.put("messages",messages);
+        model.addAttribute("messages",messages);
 
         return "main";
     }
